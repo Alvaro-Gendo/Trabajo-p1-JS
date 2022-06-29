ec389869dd4d099b8b0f3091ab15d4679a039391
Alvaro <zhuone@hotmail.com>
2022-06-29 20:57:31 -0300

    Comenzando la practica, falto punto 4

---
+++ js/EjerciciosParte1.js
@@ -0,0 +1,11 @@
+//Punto 1-Alerta
+alert("Un mensaje");
+
+//Punto 2-Hello World
+document.write("<h1>Hola Mundo</h1>");
+
+//Punto 3-Suma
+document.write("Resultado:"+(3+5));
+
+//Punto 4-Nombre de usuario
+prompt("Como te llamas?")
