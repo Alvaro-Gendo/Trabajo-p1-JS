197b19943c42f53aeff0cea7b7b3ff05530b6e16
Alvaro <zhuone@hotmail.com>
2022-07-03 17:53:29 -0300

    Terminado el ejercicio 8

---
+++ js/Ejercicio8.js
@@ -0,0 +1,9 @@
+//Ejercicio numero 8 division
+
+let numero1 = parseInt(prompt("Ingrese un número"))
+
+if(numero1 % 2 === 0){
+    document.write("El número ingresado es disivible en 2")
+}else{
+    document.write("El número ingresado no es divisible en 2")
+}
