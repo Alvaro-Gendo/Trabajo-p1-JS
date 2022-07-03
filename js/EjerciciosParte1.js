ca7e263f46f8d21ce7a55922efda7d79343b9418
Alvaro <zhuone@hotmail.com>
2022-07-03 16:14:19 -0300

    Terminado los primeros 5 puntos

--- js/EjerciciosParte1.js
+++ js/EjerciciosParte1.js
@@ -4,8 +4,17 @@ alert("Un mensaje");
 //Punto 2-Hello World
 document.write("<h1>Hola Mundo</h1>");
 
-//Punto 3-Suma
+//Punto 3-Suma simple
 document.write("Resultado:"+(3+5));
 
 //Punto 4-Nombre de usuario
-prompt("Como te llamas?")
+let nombre = prompt("Ingrese su nombre");
+
+document.write("<h2>Hola " + nombre + "</h2>");
+
+//Punto 5- Suma
+
+let numero1 = parseInt(prompt("Ingrese un numero"));
+let numero2 = parseInt(prompt("Ingrese otro numero"));
+
+document.write("Resultado: " + (numero1 + numero2));
