f5ce5a5684b414fc7fb43dfba5b4cb4f5f8eb5b9
Alvaro <zhuone@hotmail.com>
2022-07-06 19:50:07 -0300

    Ejercicio 11

--- js/Ejercicio11.js
+++ js/Ejercicio11.js
@@ -1,14 +1,16 @@
-//Ejercicio 10 divisiones 2, 3, 5, 7 en todos ellos
+//Ejercicio 11 divisiones 2, 3, 5, 7 en todos ellos
 
 let numero = parseInt(prompt("Ingrese un número"))
 
 if(numero % 2 === 0){
-    document.write("El número ingresado es disivible en 2")
- }else if(numero % 3 === 0){
-     document.write("El número ingresado es divisible en 3")
- }else if(numero % 5 === 0){
-     document.write("El número ingresado es divisible en 5")
- }else{
-     document.write("El número ingresado es divisible en 7")
- 
+    document.write("El número ingresado es disivible en 2<br>")
+}
+if(numero % 3 === 0){
+    document.write("El número ingresado es divisible en 3<br>")
+}
+if(numero % 5 === 0){
+    document.write("El número ingresado es divisible en 5<br>")
+}
+if(numero % 7 === 0){
+    document.write("El número ingresado es divisible en 7<br>")
 }
