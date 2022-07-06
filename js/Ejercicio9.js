c530c6b145dd594121c7f1a53452cd038a2ef7a0
Alvaro <zhuone@hotmail.com>
2022-07-05 21:41:54 -0300

    completo el ejercicio 9 con condicionales

--- js/Ejercicio9.js
+++ js/Ejercicio9.js
@@ -1,3 +1,41 @@
 //Ejercicio 9 vocales
 
-let frases = (prompt("Escribe una frase"))
+let frases = prompt ("Ingrese una frase de maximo 10 caracteres");
+ console.log(frases)
+ console.log(frases.length)
+ console.log(frases.charAt(0))
+ console.log(frases.charAt(frases.length-1))
+ console.log(frases.toLowerCase())
+
+frases = frases.toLowerCase();
+
+if(frases.charAt(0) === "a" || frases.charAt(0) === "e" || frases.charAt(0) === "i" ||frases.charAt(0) === "o" ||frases.charAt(0) === "u"){
+ document.write(frases.charAt(0))
+}
+if(frases.charAt(1) === "a" || frases.charAt(1) === "e" || frases.charAt(1) === "i" ||frases.charAt(1) === "o" ||frases.charAt(1) === "u"){
+ document.write(frases.charAt(1))
+}
+if(frases.charAt(2) === "a" || frases.charAt(2) === "e" || frases.charAt(2) === "i" ||frases.charAt(2) === "o" ||frases.charAt(2) === "u"){
+ document.write(frases.charAt(2))
+}
+if(frases.charAt(3) === "a" || frases.charAt(3) === "e" || frases.charAt(3) === "i" ||frases.charAt(3) === "o" ||frases.charAt(3) === "u"){
+ document.write(frases.charAt(3))
+}
+if(frases.charAt(4) === "a" || frases.charAt(4) === "e" || frases.charAt(4) === "i" ||frases.charAt(4) === "o" ||frases.charAt(4) === "u"){
+ document.write(frases.charAt(4))
+}
+if(frases.charAt(5) === "a" || frases.charAt(5) === "e" || frases.charAt(5) === "i" ||frases.charAt(5) === "o" ||frases.charAt(5) === "u"){
+ document.write(frases.charAt(5))
+}
+if(frases.charAt(6) === "a" || frases.charAt(6) === "e" || frases.charAt(6) === "i" ||frases.charAt(6) === "o" ||frases.charAt(6) === "u"){
+ document.write(frases.charAt(6))
+}
+if(frases.charAt(7) === "a" || frases.charAt(7) === "e" || frases.charAt(7) === "i" ||frases.charAt(7) === "o" ||frases.charAt(7) === "u"){
+ document.write(frases.charAt(7))
+}
+if(frases.charAt(8) === "a" || frases.charAt(8) === "e" || frases.charAt(8) === "i" ||frases.charAt(8) === "o" ||frases.charAt(8) === "u"){
+ document.write(frases.charAt(8))
+}
+if(frases.charAt(9) === "a" || frases.charAt(9) === "e" || frases.charAt(9) === "i" ||frases.charAt(9) === "o" ||frases.charAt(9) === "u"){
+ document.write(frases.charAt(9))
+}
